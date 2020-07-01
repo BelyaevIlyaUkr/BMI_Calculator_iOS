a8b5b30caeb50f75be7a1eee50d096d71dc9270a
Ilya Belyaev <60135683+BelyaevIlyaUkr@users.noreply.github.com>
2020-07-02 02:28:39 +0300

    Initial commit

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var calculatorBrain = CalculatorBrain()
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.fKg", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        calculatorBrain.calculateBMI(height: heightSlider.value, weight: weightSlider.value)
+        performSegue(withIdentifier: "goToResult", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult" {
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = calculatorBrain.getBMIValue()
+            destinationVC.advice = calculatorBrain.getAdvice()
+            destinationVC.color = calculatorBrain.getColor()
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# Body Mass Index calculator. 
+
+Based on the user’s weight and height it will calculate their body mass index and give a piece of health advice depending on whether if they have eaten too many pies or if they need to eat more pies.
+
+![alt_text](Screenshots/WeightAndHeightSelection.png)
+# 
+![alt_text](Screenshots/EatMorePies.png)
+#
+![alt_text](Screenshots/FitAsFiddle.png)
+#
+![alt_text](Screenshots/EatLessPies.png)
